c9050df95dc71322d6d856f05c737b00b3f08ec8
gnsdp <rladpgns99@gmail.com>
2025-12-03 11:37:10 +0900

    test: UserApiControllerTest 수정

--- src/test/java/ktb3/full/community/presentation/controller/UserApiControllerTest.java
+++ src/test/java/ktb3/full/community/presentation/controller/UserApiControllerTest.java
@@ -1,138 +1,28 @@
 package ktb3.full.community.presentation.controller;
 
-import ktb3.full.community.common.exception.DuplicatedEmailException;
-import ktb3.full.community.common.exception.DuplicatedNicknameException;
-import ktb3.full.community.common.exception.UserNotFoundException;
-import ktb3.full.community.dto.request.UserRegisterRequest;
-import ktb3.full.community.dto.response.UserProfileResponse;
-import ktb3.full.community.dto.response.UserValidationResponse;
+import ktb3.full.community.ControllerTestSupport;
 import ktb3.full.community.fixture.MultipartFileFixture;
-import ktb3.full.community.service.UserService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(
-        controllers = UserApiController.class,
-        excludeAutoConfiguration = SecurityAutoConfiguration.class)
-class UserApiControllerTest {
-
-    private static final String BASE_URI = "/users";
-
-    @MockitoBean
-    private UserService userService;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Nested
-    class validateEmailAvailable {
-
-        String TARGET_URI = BASE_URI + "/email-validation";
-
-        @Test
-        void 존재하지_않는_이메일이면_200_Ok와_true를_응답한다() throws Exception {
-            // given
-            String email = "email@example.com";
-            UserValidationResponse response = new UserValidationResponse(true);
-
-            when(userService.validateEmailAvailable(email)).thenReturn(response);
-
-            // when
-            ResultActions resultActions = mockMvc.perform(get(TARGET_URI)
-                    .param("email", email));
-
-            // then
-            resultActions
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.available").value(true));
-        }
-
-        @Test
-        void 이미_존재하는_이메일이면_200_Ok와_false를_응답한다() throws Exception {
-            // given
-            String email = "email@example.com";
-            UserValidationResponse response = new UserValidationResponse(false);
-
-            when(userService.validateEmailAvailable(email)).thenReturn(response);
-
-            // when
-            ResultActions resultActions = mockMvc.perform(get(TARGET_URI)
-                    .param("email", email));
-
-            // then
-            resultActions
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.available").value(false));
-        }
-    }
-
-    @Nested
-    class validateNicknameAvailable {
-
-        String TARGET_URI = BASE_URI + "/nickname-validation";
-
-        @Test
-        void 존재하지_않는_닉네임이면_200_Ok와_true를_응답한다() throws Exception {
-            // given
-            String nickname = "name";
-            UserValidationResponse response = new UserValidationResponse(true);
-
-            when(userService.validateNicknameAvailable(nickname)).thenReturn(response);
-
-            // when
-            ResultActions resultActions = mockMvc.perform(get(TARGET_URI)
-                    .param("nickname", nickname));
-
-            // then
-            resultActions
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.available").value(true));
-        }
-
-        @Test
-        void 이미_존재하는_닉네임이면_200_Ok와_false를_응답한다() throws Exception {
-            // given
-            String nickname = "name";
-            UserValidationResponse response = new UserValidationResponse(false);
-
-            when(userService.validateNicknameAvailable(nickname)).thenReturn(response);
-
-            // when
-            ResultActions resultActions = mockMvc.perform(get(TARGET_URI)
-                    .param("nickname", nickname));
-
-            // then
-            resultActions
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.available").value(false));
-        }
-    }
+class UserApiControllerTest extends ControllerTestSupport {
 
     @Nested
     class signUp {
 
-        String TARGET_URI = BASE_URI;
-
         @Test
-        void 회원가입에_성공하면_201_Created를_응답한다() throws Exception {
+        void 회원을_가입한다() throws Exception {
             // given
-            when(userService.register(any(UserRegisterRequest.class))).thenReturn(1L);
 
             // when
-            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, TARGET_URI)
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/users")
                     .file(MultipartFileFixture.createProfileImage())
                     .param("email", "email@example.com")
                     .param("password", "Password123!")
@@ -140,101 +30,72 @@ class UserApiControllerTest {
 
             // then
             resultActions
+                    .andDo(print())
                     .andExpect(status().isCreated())
-                    .andExpect(header().string(HttpHeaders.LOCATION, "/users/1"));
+                    .andExpect(header().exists(HttpHeaders.LOCATION))
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
         }
 
         @Test
-        void 입력값이_유효하지_않으면_400_BadRequest와_4001을_응답한다() throws Exception {
+        void 회원가입_시_이메일에_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
             // given
 
             // when
-            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, TARGET_URI)
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/users")
                     .file(MultipartFileFixture.createProfileImage())
-                    .param("email", "email.com")
-                    .param("password", "password123!")
-                    .param("nickname", "longNickname"));
+                    .param("email", " ")
+                    .param("password", "Password123!")
+                    .param("nickname", "name"));
 
             // then
             resultActions
+                    .andDo(print())
                     .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.code").value(4001));
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("이메일은 필수입니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
         }
 
         @Test
-        void 이미_존재하는_이메일이면_409_Conflict와_4091을_응답한다() throws Exception {
+        void 회원가입_시_비밀번호에_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
             // given
-            when(userService.register(any(UserRegisterRequest.class))).thenThrow(DuplicatedEmailException.class);
 
             // when
-            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, TARGET_URI)
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/users")
                     .file(MultipartFileFixture.createProfileImage())
                     .param("email", "email@example.com")
-                    .param("password", "Password123!")
+                    .param("password", " ")
                     .param("nickname", "name"));
 
             // then
             resultActions
-                    .andExpect(status().isConflict())
-                    .andExpect(jsonPath("$.code").value(4091));
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("비밀번호는 필수입니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
         }
 
         @Test
-        void 이미_존재하는_닉네임이면_409_Conflict와_4092를_응답한다() throws Exception {
+        void 회원가입_시_닉네임에_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
             // given
-            when(userService.register(any(UserRegisterRequest.class))).thenThrow(DuplicatedNicknameException.class);
 
             // when
-            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, TARGET_URI)
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/users")
                     .file(MultipartFileFixture.createProfileImage())
                     .param("email", "email@example.com")
                     .param("password", "Password123!")
-                    .param("nickname", "name"));
+                    .param("nickname", " "));
 
             // then
             resultActions
-                    .andExpect(status().isConflict())
-                    .andExpect(jsonPath("$.code").value(4092));
-        }
-    }
-
-    @Nested
-    class getUserProfile {
-
-        String TARGET_URI = BASE_URI + "/{userId}";
-
-        @Test
-        void 회원이_존재하면_200_Ok와_프로필_정보를_응답한다() throws Exception {
-            // given
-            long userId = 1L;
-            UserProfileResponse response = new UserProfileResponse(userId, "name", "profileImage.png");
-
-            when(userService.getUserProfile(userId)).thenReturn(response);
-
-            // when
-            ResultActions resultActions = mockMvc.perform(get(TARGET_URI, userId));
-
-            // then
-            resultActions
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.userId").value(userId))
-                    .andExpect(jsonPath("$.data.nickname").value("name"))
-                    .andExpect(jsonPath("$.data.profileImageName").value("profileImage.png"));
-        }
-
-        @Test
-        void 존재하지_않는_회원이면_404_NotFound와_4041을_응답한다() throws Exception {
-            // given
-            long userId = 1L;
-            when(userService.getUserProfile(userId)).thenThrow(UserNotFoundException.class);
-
-            // when
-            ResultActions resultActions = mockMvc.perform(get(TARGET_URI, userId));
-
-            // then
-            resultActions
-                    .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.code").value(4041));
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("닉네임은 필수입니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
         }
     }
 }
