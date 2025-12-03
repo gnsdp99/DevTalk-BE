0607268cbb322dc491a21d76e4169061f199b263
gnsdp <rladpgns99@gmail.com>
2025-12-03 14:20:16 +0900

    test: AuthUserDetailsService 통합 테스트

--- src/test/java/ktb3/full/community/security/userdetails/AuthUserDetailsServiceTest.java
+++ src/test/java/ktb3/full/community/security/userdetails/AuthUserDetailsServiceTest.java
@@ -1,42 +1,40 @@
 package ktb3.full.community.security.userdetails;
 
-import ktb3.full.community.config.UnitTest;
-import ktb3.full.community.domain.entity.User;
+import ktb3.full.community.IntegrationTestSupport;
 import ktb3.full.community.fixture.UserFixture;
 import ktb3.full.community.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.BDDMockito.given;
 
-@UnitTest
-class AuthUserDetailsServiceTest {
+class AuthUserDetailsServiceTest extends IntegrationTestSupport {
 
-    @Mock
+    @Autowired
     private UserRepository userRepository;
 
-    @InjectMocks
-    private AuthUserDetailsService userDetailsService;
+    @Autowired
+    private AuthUserDetailsService sut;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAllInBatch();
+    }
 
     @Nested
     class loadUserByUsername {
 
         @Test
-        void 이메일이_존재하면_조회에_성공한다() {
+        void 이메일이_존재하면_회원이_조회된다() {
             // given
-            User user = UserFixture.createUser("email@example.com", "Password123!", "name", "profileImageName", false);
-
-            given(userRepository.findByEmail("email@example.com")).willReturn(Optional.of(user));
+            userRepository.save(UserFixture.createUser("email@example.com", "Password123!", "name", "profileImageName", false));
 
             // when
-            AuthUserDetails userDetails = (AuthUserDetails) userDetailsService.loadUserByUsername("email@example.com");
+            AuthUserDetails userDetails = (AuthUserDetails) sut.loadUserByUsername("email@example.com");
 
             // then
             assertThat(userDetails.getUsername()).isEqualTo("email@example.com");
@@ -47,22 +45,19 @@ class AuthUserDetailsServiceTest {
         @Test
         void 이메일이_존재하지_않으면_예외가_발생한다() {
             // given
-            given(userRepository.findByEmail("email@example.com")).willReturn(Optional.empty());
 
             // when & then
-            assertThatThrownBy(() -> userDetailsService.loadUserByUsername("email@example.com"))
+            assertThatThrownBy(() -> sut.loadUserByUsername("email@example.com"))
                     .isInstanceOf(UsernameNotFoundException.class);
         }
 
         @Test
         void 이메일이_존재해도_탈퇴한_회원이면_예외가_발생한다() {
             // given
-            User user = UserFixture.createDeletedWithEmail("email@example.com");
-
-            given(userRepository.findByEmail("email@example.com")).willReturn(Optional.of(user));
+            userRepository.save(UserFixture.createDeletedWithEmail("email@example.com"));
 
             // when & then
-            assertThatThrownBy(() -> userDetailsService.loadUserByUsername("email@example.com"))
+            assertThatThrownBy(() -> sut.loadUserByUsername("email@example.com"))
                     .isInstanceOf(UsernameNotFoundException.class);
         }
     }
